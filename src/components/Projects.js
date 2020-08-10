d8db27f8c9c389d1134e4ed359d4c5f6bf4ecee1
Michael Walter <michael@MacBook-Pro.local>
2020-08-10 13:34:27 -0700

    Working on scaling.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,19 +6,19 @@ const Projects = () => {
     return (
         <div className="projects-container">
             <div>
-                <h2>Conway's Game of Life</h2>
+                <h3>Conway's Game of Life</h3>
                 (<a href={"https://friendly-wozniak-c97571.netlify.app/"} rel={"_blank"}>Live</a>)
 
                 <p><strong>Stack:</strong> React with hooks, JavaScript, React-Bootstrap, HTML and CSS</p>
             </div>
             <div>
-                <h2>International School Social Worker </h2>
+                <h3>International School Social Worker </h3>
                 (<a href={"https://github.com/M-PAW/Front-end/tree/master/school-social-worker"} rel={"_blank"}>Github</a>)
 
                 <p><strong>Stack:</strong> React, Redux, React Router, Axios, Formik, JavaScript, Styled-Components</p>
             </div>
             <div>
-                <h2>MyDish </h2>
+                <h3>MyDish </h3>
                 (<a href={"https://github.com/M-PAW-LAMBDA-REVIEW/mydish-be"} rel={"_blank"}>Github</a>)
 
                 <p><strong>Stack:</strong> React-Native, Jest, Express, NodeJS, JavaScript, HTML, CSS, KnexJS, PostgreSQL</p>

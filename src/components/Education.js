a2dd15a6f56f369506313e0fd7b99887a56ec88a
Michael Walter <michael@MacBook-Pro.local>
2020-08-10 08:37:59 -0700

    Updated flex and scaling

--- src/components/Education.js
+++ src/components/Education.js
@@ -8,28 +8,28 @@ const Education = () => {
 
         <div className="education-container">
             <div>
-                <h3>Formal Education</h3>
+                <h2>Formal Education</h2>
                 <section>   
-                    <h5>Lambda School</h5>
+                    <h4>Lambda School</h4>
                     <p>Full-Stack Development</p>
                 </section>
                 <section>
-                    <h5>Nanjing University</h5>
+                    <h4>Nanjing University</h4>
                     <p>Mandarin Chinese</p>
                 </section>
                 <section>
-                    <h5>Riverside City College</h5>
+                    <h4>Riverside City College</h4>
                     <p>Computer Science</p>
                 </section>
             </div>
             <div>
-                <h3>Informal Education</h3>
+                <h2>Informal Education</h2>
                 <section>
                     <h5>Udemy</h5>
                 </section>
             </div>
             <div>
-                <h3>Code Challenges</h3>
+                <h2>Code Challenges</h2>
                 <section>
                     <h5>leetcode</h5>
                 </section>

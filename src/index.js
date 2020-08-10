3cd26b470700bab84bd17d23ffb28c74b73811fd
Michael Walter <michael@MacBook-Pro.local>
2020-08-09 19:32:22 -0700

    Hating this deployment.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter as Router} from "react-router-dom";
-import { Route } from "react-router-dom";
 import Navigation from './components/Navigation';
 
 ReactDOM.render(

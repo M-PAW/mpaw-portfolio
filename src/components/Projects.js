deaba9c1de05328abccec4718a53600e61fbee7a
Michael Walter <michael@MacBook-Pro.local>
2020-08-11 13:57:47 -0700

    Adjusting styles for projects component.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,21 +5,21 @@ import '../styles/projects.css';
 const Projects = () => {
     return (
         <div className="projects-container">
-            <h2>Projects</h2>
+            <h3>Projects</h3>
             <div>
-                <h3>Conway's Game of Life</h3>
+                <h4>Conway's Game of Life</h4>
                 (<a href={"https://friendly-wozniak-c97571.netlify.app/"} rel={"_blank"}>Live</a>)
 
                 <p><strong>Stack:</strong> React with hooks, JavaScript, React-Bootstrap, HTML and CSS</p>
             </div>
             <div>
-                <h3>International School Social Worker </h3>
+                <h4>International School Social Worker </h4>
                 (<a href={"https://github.com/M-PAW/Front-end/tree/master/school-social-worker"} rel={"_blank"}>Github</a>)
 
                 <p><strong>Stack:</strong> React, Redux, React Router, Axios, Formik, JavaScript, Styled-Components</p>
             </div>
             <div>
-                <h3>MyDish </h3>
+                <h4>MyDish </h4>
                 (<a href={"https://github.com/M-PAW-LAMBDA-REVIEW/mydish-be"} rel={"_blank"}>Github</a>)
 
                 <p><strong>Stack:</strong> React-Native, Jest, Express, NodeJS, JavaScript, HTML, CSS, KnexJS, PostgreSQL</p>

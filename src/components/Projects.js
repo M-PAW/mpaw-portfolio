d05ad0aa45dc27ad67fc5a68f953a35274ca9a43
Michael Walter <michael@MacBook-Pro.local>
2020-08-13 14:28:30 -0700

    Fixed style on projects component.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -36,7 +36,7 @@ const Projects = () => {
                     <Paper className={classes.paper}  elevation={0}>
                         <h5>Conway's Game of Life</h5>
                     (<a href={"https://friendly-wozniak-c97571.netlify.app/"} rel={"_blank"}>Live</a>) (<a href={"https://github.com/M-PAW/conways-game-of-life"}>Github</a>)
-                    <p className="project-desc">A unique recreation of Conway's Game of Life.</p>
+                    <p className="project-desc" style={{marginBottom: "30px"}}>A unique recreation of Conway's Game of Life.</p>
                     <p className="stack"><strong>Stack:</strong> React with hooks, JavaScript, React-Bootstrap, HTML and CSS</p>
                     </Paper>
                 </Grid>
@@ -45,7 +45,7 @@ const Projects = () => {
                     <Paper className={classes.paper}  elevation={0}>
                         <h5>International School Social Worker </h5>
                     (<a href={"https://github.com/M-PAW/Front-end/tree/master/school-social-worker"} rel={"_blank"}>Github</a>)
-                    <p className="project-desc">A project aimed at helping social workers and children connect.</p>
+                    <p className="project-desc" style={{marginBottom: "30px"}}>A project aimed at helping social workers and children connect.</p>
                     <p className="stack"><strong>Stack:</strong> React, Redux, React Router, Axios, Formik, JavaScript, Styled-Components</p>
                     </Paper>
                 </Grid>
@@ -54,7 +54,7 @@ const Projects = () => {
                     <Paper className={classes.paper}  elevation={0}>
                         <h5>MyDish </h5>
                     (<a href={"https://github.com/M-PAW-LAMBDA-REVIEW/mydish-be"} rel={"_blank"}>Github</a>)
-                    <p className="project-desc">A mobile app to push the bounds of cooking.</p>
+                    <p className="project-desc" style={{marginBottom: "10px"}}>A mobile app to push the bounds of cooking.</p>
                     <p><strong>Stack:</strong> React-Native, Jest, Express, NodeJS, JavaScript, HTML, CSS, KnexJS, PostgreSQL</p>
                     </Paper>
                 </Grid>

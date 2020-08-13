eaa7c286e72ddd3b47797c0fe819e265112037ba
Michael Walter <michael@MacBook-Pro.local>
2020-08-13 14:07:27 -0700

    Added switch to routing.

--- src/App.js
+++ src/App.js
@@ -5,36 +5,38 @@ import Education from './components/Education';
 import Home from './components/Home';
 import Skills from './components/Skills';
 //import Contact from './components/Contact';
+import { Switch } from 'react-router-dom';
 
 import { Route } from 'react-router-dom';
 
 const App = () => {
     return (
         <div className="route-container">
-            <Route exact path="/" >
-            <Home />
-            </Route>
+            <Switch>
+                <Route exact path="/" >
+                <Home />
+                </Route>
 
-            {/* <Route path="/About">
-            <About />
-            </Route> */}
+                {/* <Route path="/About">
+                <About />
+                </Route> */}
 
-            {/* <Route path="/Contact">
-            <Contact />
-            </Route> */}
+                {/* <Route path="/Contact">
+                <Contact />
+                </Route> */}
 
-            <Route path="/Education">
-                <Education />
-            </Route>
+                <Route path="/Education">
+                    <Education />
+                </Route>
 
-            <Route path="/Projects">
-                <Projects />
-            </Route>
+                <Route path="/Projects">
+                    <Projects />
+                </Route>
 
-            <Route path="/Skills">
-                <Skills />
-            </Route>
-        
+                <Route path="/Skills">
+                    <Skills />
+                </Route>
+            </Switch>
         </div>
     );
 };
